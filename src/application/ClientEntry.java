710d7d69915ebdf0a609fe907c534f874759d252
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-26 20:56:08 -0300

    Set exercise 01

---
+++ src/application/ClientEntry.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class ClientEntry {
+	
+	private String userName;
+	private Date moment;
+	
+	public ClientEntry(String userName, Date moment) {
+		super();
+		this.userName = userName;
+		this.moment = moment;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClientEntry other = (ClientEntry) obj;
+		return Objects.equals(userName, other.userName);
+	}
+}
